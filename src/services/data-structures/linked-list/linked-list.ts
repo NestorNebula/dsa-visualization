891412bd4c8bbbe2dd0a236188614e3d378afb55
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-15 16:12:10 +0100

    feat: add LinkedList class

---
+++ src/services/data-structures/linked-list/linked-list.ts
@@ -0,0 +1,43 @@
+class Node<Type> {
+  value: Type;
+  next: Node<Type> | null = null;
+
+  constructor(value: Type) {
+    this.value = value;
+  }
+}
+
+class LinkedList<Type = any> {
+  head: Node<Type> | null = null;
+  tail: Node<Type> | null = null;
+  add(value: Type): void {
+    const node = new Node(value);
+    if (!this.head || !this.tail) {
+      this.head = this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+  remove(value: Type): Node<Type> | undefined {
+    let previous: Node<Type> | null = null;
+    let node: Node<Type> | null = this.head;
+    while (node) {
+      if (node.value === value) {
+        if (previous) {
+          previous.next = node.next;
+        } else {
+          this.head = node.next;
+        }
+        if (!node.next) {
+          this.tail = null;
+        }
+        return node;
+      }
+      previous = node;
+      node = node.next;
+    }
+  }
+}
+
+export default LinkedList;
