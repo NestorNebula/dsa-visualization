67c952d03d72ce2b282407b18b1d24e4cca34a9d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-15 15:15:19 +0100

    test: add tests for Array class

---
+++ src/services/data-structures/array/array.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from 'vitest';
+import Array from './array';
+
+describe('array', () => {
+  console.log(0);
+  const array = new Array();
+  it('creates a new array', () => {
+    expect(array.length).toBe(0);
+    expect(array[0]).toBeUndefined();
+  });
+
+  describe('push', () => {
+    it('pushes new data to the end of the array', () => {
+      array.push('Test');
+      expect(array.length).toBe(1);
+    });
+  });
+
+  describe('pop', () => {
+    it('removes item from the end of the array', () => {
+      const item = array.pop();
+      expect(array.length).toBe(0);
+      expect(item).toBe('Test');
+    });
+  });
+
+  it('lets user access an item at a particular index', () => {
+    array.push('Item one');
+    array.push('Item two');
+    expect(array[0]).toBe('Item one');
+    expect(array[1]).toBe('Item two');
+  });
+
+  describe('shift', () => {
+    it('removes item from the start of the array', () => {
+      const item = array.shift();
+      expect(array.length).toBe(1);
+      expect(item).toBe('Item one');
+    });
+  });
+
+  it('updates item at a particular index', () => {
+    array[0] = 'Item one';
+    expect(array[0]).toBe('Item one');
+  });
+});
