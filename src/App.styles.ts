c40e1e2c1b049a7c3f142cb53d25c230f2fbf33a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-17 11:55:35 +0100

    feat: add content to App

---
+++ src/App.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const App = styled.main``;
+const Header = styled.header``;
+const Main = styled.main``;
+const Links = styled.div``;
+
+export { App, Header, Main, Links };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import { Link } from 'react-router';
+import * as S from './App.styles';
+
 function App() {
-  return <></>;
+  return (
+    <S.App>
+      <S.Header>Welcome to DSA Visualization!</S.Header>
+      <S.Main>
+        <div>
+          This website was designed to help you visualize data structures and
+          algorithms more easily.
+        </div>
+        <div>
+          You will be able to see many data structures representations like
+          Arrays, Binary Trees, Hashmaps... and to perform operations on them to
+          see how they evolve.
+        </div>
+        <div>
+          You will also see algorithms in action, like searching and sorting
+          algorithms performed on these data structures.
+        </div>
+        <div>
+          Each data structure can contain different values, that you can add,
+          remove or change depending on the data structure. You will also be
+          able to create multiple variations of the same data structures to test
+          them with different values.
+        </div>
+        <div>
+          Everything you create or change will be saved for you to continue your
+          work when you come back to the website.
+        </div>
+        <div>
+          Each data structure/algorithm page will link to a resource to learn
+          more about it.
+        </div>
+        <S.Links>
+          <div>Start now!</div>
+          <Link to="/data-structures">Data Structures</Link>
+          <Link to="/algorithms">Algorithms</Link>
+        </S.Links>
+      </S.Main>
+    </S.App>
+  );
 }
 
 export default App;
