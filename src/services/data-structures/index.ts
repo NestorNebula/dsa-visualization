8903c9f90e6635500d8a1b5dfe48410d02f9c7de
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-17 10:12:07 +0100

    feat: export all data structures from index file

---
+++ src/services/data-structures/index.ts
@@ -0,0 +1,10 @@
+import Array from './array/array';
+import BinaryTree from './binary-tree/binary-tree';
+import Graph from './graph/graph';
+import HashMap from './hashmap/hashmap';
+import Heap from './heap/heap';
+import LinkedList from './linked-list/linked-list';
+import Queue from './queue/queue';
+import Stack from './stack/stack';
+
+export { Array, BinaryTree, Graph, HashMap, Heap, LinkedList, Queue, Stack };
