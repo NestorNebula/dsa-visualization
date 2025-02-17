d8058db696b13949e87547f967be121622aaa196
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-17 16:34:35 +0100

    feat: add PageLink component

---
+++ src/components/page-link/PageLink.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const PageLink = styled.button``;
+const Title = styled.div``;
+const Icon = styled.img``;
+
+export { PageLink, Title, Icon };
---
+++ src/components/page-link/PageLink.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router';
+import * as S from './PageLink.styles';
+
+function PageLink({
+  title,
+  icon,
+  path,
+}: {
+  title: string;
+  icon: string;
+  path: string;
+}) {
+  const navigate = useNavigate();
+  const go = () => navigate(path);
+  return (
+    <S.PageLink onClick={go}>
+      <S.Title>{title}</S.Title>
+      <S.Icon src={icon} alt={`${title.toLowerCase()}'s icon`} />
+    </S.PageLink>
+  );
+}
+
+export default PageLink;
