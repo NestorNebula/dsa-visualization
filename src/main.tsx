6c50f1c8f6a8423fdf9f00063ea8d90dbc8819d4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-17 17:10:03 +0100

    feat: add Navbar to routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import routes from '@routes/routes';
-import Navbar from '@pages/navbar/Navbar';
 import Footer from '@pages/footer/Footer';
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Navbar />
     <RouterProvider router={router} />
     <Footer />
   </StrictMode>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,5 @@
 import type { RouteObject } from 'react-router';
+import Navbar from '@pages/navbar/Navbar';
 import App from '../App';
 import DataStructures from '@pages/data-structures/DataStructures';
 import Error from '@pages/error/Error';
@@ -6,21 +7,28 @@ import Sperror from 'sperror';
 
 const routes: RouteObject[] = [
   {
-    index: true,
-    element: <App />,
+    path: '/',
+    element: <Navbar />,
     errorElement: <Error />,
-  },
-  {
-    path: '/data-structures',
-    element: <DataStructures />,
-    errorElement: <Error />,
-  },
-  {
-    path: '*',
-    errorElement: <Error />,
-    loader: () => {
-      throw new Sperror('Not Found', 'Page not found', 404);
-    },
+    children: [
+      {
+        index: true,
+        element: <App />,
+        errorElement: <Error />,
+      },
+      {
+        path: '/data-structures',
+        element: <DataStructures />,
+        errorElement: <Error />,
+      },
+      {
+        path: '*',
+        errorElement: <Error />,
+        loader: () => {
+          throw new Sperror('Not Found', 'Page not found', 404);
+        },
+      },
+    ],
   },
 ];
 
