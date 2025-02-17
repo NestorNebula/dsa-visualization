fa3b9e21f2926b9efd1a7e8e3f5dafe1d0f698ba
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-17 11:29:35 +0100

    feat: add Error page

---
+++ src/pages/error/Error.tsx
@@ -0,0 +1,24 @@
+import { Link, useRouteError } from 'react-router';
+import Err from '@components/error/Error';
+import Sperror from 'sperror';
+import * as S from './Error.styles';
+
+function Error() {
+  const error = useRouteError();
+  return error instanceof Sperror ? (
+    <S.Error>
+      <Err error={error} />
+      {error.statusCode === 404 ? (
+        <Link to="/">Go back to home page</Link>
+      ) : (
+        <></>
+      )}
+    </S.Error>
+  ) : (
+    <S.Error>
+      <Err error={error} />
+    </S.Error>
+  );
+}
+
+export default Error;
