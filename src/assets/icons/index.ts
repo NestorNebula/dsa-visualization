fef30dd16e44ce38c70e5621a4447c1b3eb5aa52
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-17 17:48:44 +0100

    feat: add resource icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,6 @@
 import arrowLeft from './arrow-left.svg';
 import github from './github.svg';
 import icon from './icon.png';
+import resource from './resource.svg';
 
-export { arrowLeft, github, icon };
+export { arrowLeft, github, icon, resource };
