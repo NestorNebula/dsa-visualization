506b87bd328644101b984d0e785dadc5b2aad4f4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-18 08:33:16 +0100

    feat: add close icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,7 @@
 import arrowLeft from './arrow-left.svg';
+import close from './close.svg';
 import github from './github.svg';
 import icon from './icon.png';
 import resource from './resource.svg';
 
-export { arrowLeft, github, icon, resource };
+export { arrowLeft, close, github, icon, resource };
