54f7e58f534a16c8eefd7ba0ed8c0f5a606a5f79
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-18 11:14:09 +0100

    feat: add plus icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,6 +2,7 @@ import arrowLeft from './arrow-left.svg';
 import close from './close.svg';
 import github from './github.svg';
 import icon from './icon.png';
+import plus from './plus.svg';
 import resource from './resource.svg';
 
-export { arrowLeft, close, github, icon, resource };
+export { arrowLeft, close, github, icon, plus, resource };
