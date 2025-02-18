3f21489db9ca4836b68c5c2d6ff91b1d119400e5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-18 12:07:37 +0100

    feat: add function to check dataType
    This will avoid storing or using wrong values

--- src/hooks/use-local-storage/useLocalStorage.ts
+++ src/hooks/use-local-storage/useLocalStorage.ts
@@ -1,17 +1,36 @@
 import { useState } from 'react';
 
-const useLocalStorage = (key: string, def: any[]) => {
+function useLocalStorage<Type = any>(key: string, def: Type[]) {
+  function checkType(def: any[], data: any[]): boolean {
+    const defType = typeof def;
+    const dataType = typeof data;
+    if (defType !== dataType) return false;
+    if (defType !== 'object') return true;
+    const defKeys = Object.keys(def).sort();
+    const dataKeys = Object.keys(data).sort();
+    for (let i = 0; i < defKeys.length; i++) {
+      if (
+        defKeys[i] !== dataKeys[i] ||
+        typeof def[0][defKeys[i]] !== typeof data[0][dataKeys[i]]
+      )
+        return false;
+    }
+    return true;
+  }
+
   const storedData = JSON.parse(localStorage.getItem(key) ?? '[]');
-  const [data, setData] = useState<any[]>(
-    Array.isArray(storedData) && storedData.length ? storedData : def
+  const [data, setData] = useState<Type[]>(
+    Array.isArray(storedData) && storedData.length && checkType(def, storedData)
+      ? storedData
+      : def
   );
-  const update = (data: any[]) => {
-    const updatedData = data.length ? data : def;
+  const update = (data: Type[]) => {
+    const updatedData = data.length && checkType(def, data) ? data : def;
     setData(updatedData);
     localStorage.setItem(key, JSON.stringify(updatedData));
   };
 
   return { data, update };
-};
+}
 
 export default useLocalStorage;
