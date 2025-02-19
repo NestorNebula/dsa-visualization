cd1449478b6d8ecf5da6f3d9a89bdcc89128196d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-19 11:38:00 +0100

    test: add test for click on box

--- src/components/box/Box.test.tsx
+++ src/components/box/Box.test.tsx
@@ -1,10 +1,22 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Box from './Box';
+
+const mockClick = vi.fn();
+
+beforeEach(() => {
+  render(<Box value={1} onClick={mockClick} />);
+});
 
 describe('box', () => {
   it("renders box's value", () => {
-    render(<Box value={1} />);
     expect(screen.queryByText(1)).toBeInTheDocument();
+  });
+
+  it('calls onClick on click', async () => {
+    const user = userEvent.setup();
+    await user.click(screen.getByText(1));
+    expect(mockClick).toHaveBeenCalled();
   });
 });
