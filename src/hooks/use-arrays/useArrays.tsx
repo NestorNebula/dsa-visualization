de034dcafd3153882a6afd9f04baede5cd75df13
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-19 18:19:05 +0100

    feat: add push, shift and pop methods

--- src/hooks/use-arrays/useArrays.tsx
+++ src/hooks/use-arrays/useArrays.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import useLocalStorage from '@hooks/use-local-storage/useLocalStorage';
 import { dataStructures } from '@services/default';
+import { Array } from '@services/data-structures';
 import { array as methods } from '@services/methods';
-import type { Array } from '@services/data-structures';
 
 function useArrays() {
   const { data: arrays, update } = useLocalStorage(
     'arrays',
-    dataStructures.arrays
+    dataStructures.arrays,
+    (stored: Array) => new Array(stored)
   );
   const [array, setArray] = useState(0);
   const addArray = () => {
@@ -16,12 +17,32 @@ function useArrays() {
   const removeArray = (index: number) => {
     update(methods.remove(arrays, index));
   };
+  const pushItem = (value: any) => {
+    arrays[array].push(value);
+    update(arrays);
+  };
+  const shiftItem = () => {
+    arrays[array].shift();
+    update(arrays);
+  };
+  const popItem = () => {
+    arrays[array].pop();
+    update(arrays);
+  };
 
   const [item, setItem] = useState<number | null>(null);
 
   return {
     arrays,
-    array: { active: array, set: setArray, add: addArray, remove: removeArray },
+    array: {
+      active: array,
+      set: setArray,
+      add: addArray,
+      remove: removeArray,
+      push: pushItem,
+      shift: shiftItem,
+      pop: popItem,
+    },
     item: { active: item, set: setItem },
   };
 }
