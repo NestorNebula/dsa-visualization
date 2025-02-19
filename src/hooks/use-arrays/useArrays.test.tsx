d36b5411babf04e542151149e4c16085e67725dd
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-19 16:18:45 +0100

    test: add tests for add and remove methods

--- src/hooks/use-arrays/useArrays.test.tsx
+++ src/hooks/use-arrays/useArrays.test.tsx
@@ -1,22 +1,33 @@
 import { describe, expect, it } from 'vitest';
 import { renderHook, act } from '@testing-library/react';
 import useArrays from './useArrays';
-import { dataStructures } from '@services/default';
 
 describe('useArrays', () => {
-  const arrays = dataStructures.arrays;
-
   it('keeps track of active array', () => {
-    const { result } = renderHook(() => useArrays(arrays));
+    const { result } = renderHook(() => useArrays());
     expect(result.current.array.active).toBe(0);
     act(() => result.current.array.set(1));
     expect(result.current.array.active).toBe(1);
   });
 
   it('keeps track of active item', () => {
-    const { result } = renderHook(() => useArrays(arrays));
+    const { result } = renderHook(() => useArrays());
     expect(result.current.item.active).toBeNull();
     act(() => result.current.item.set(1));
     expect(result.current.item.active).toBe(1);
+  });
+
+  it('adds array to arrays', () => {
+    const { result } = renderHook(() => useArrays());
+    const length = result.current.arrays.length;
+    act(() => result.current.array.add());
+    expect(result.current.arrays).toHaveLength(length + 1);
+  });
+
+  it('removes array from arrays', () => {
+    const { result } = renderHook(() => useArrays());
+    const length = result.current.arrays.length;
+    act(() => result.current.array.remove(length - 1));
+    expect(result.current.arrays).toHaveLength(length - 1);
   });
 });
