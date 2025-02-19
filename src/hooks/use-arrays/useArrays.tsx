7b9da925f065f368d2c373d459172faa6d222ae0
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-19 16:38:05 +0100

    feat: add add/remove array methods

--- src/hooks/use-arrays/useArrays.tsx
+++ src/hooks/use-arrays/useArrays.tsx
@@ -1,12 +1,27 @@
 import { useState } from 'react';
+import useLocalStorage from '@hooks/use-local-storage/useLocalStorage';
+import { dataStructures } from '@services/default';
+import { array as methods } from '@services/methods';
 import type { Array } from '@services/data-structures';
 
-function useArrays(arrays: Array[]) {
+function useArrays() {
+  const { data: arrays, update } = useLocalStorage(
+    'arrays',
+    dataStructures.arrays
+  );
   const [array, setArray] = useState(0);
+  const addArray = () => {
+    update(methods.add(arrays));
+  };
+  const removeArray = (index: number) => {
+    update(methods.remove(arrays, index));
+  };
+
   const [item, setItem] = useState<number | null>(null);
 
   return {
-    array: { active: array, set: setArray },
+    arrays,
+    array: { active: array, set: setArray, add: addArray, remove: removeArray },
     item: { active: item, set: setItem },
   };
 }
