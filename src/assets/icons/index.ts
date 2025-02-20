11570dbb59fdc1e220e97ce11471fd767974bb3b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-20 14:30:27 +0100

    feat: add link-arrow icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,7 +3,8 @@ import check from './check.svg';
 import close from './close.svg';
 import github from './github.svg';
 import icon from './icon.png';
+import linkArrow from './link-arrow.svg';
 import plus from './plus.svg';
 import resource from './resource.svg';
 
-export { arrowLeft, check, close, github, icon, plus, resource };
+export { arrowLeft, check, close, github, icon, linkArrow, plus, resource };
