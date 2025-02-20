500d9c433f923d4919e93a6b3a36a14be527f9cd
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-20 14:44:04 +0100

    test: add tests for common methods

---
+++ src/services/methods/common/common.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, it } from 'vitest';
+import methods from './common';
+
+class Obj {
+  value = 'test';
+  constructor(value: string) {
+    this.value = value;
+  }
+}
+
+describe('common', () => {
+  const objects: Obj[] = [new Obj('First obj')];
+
+  describe('add', () => {
+    it('adds new object to array', () => {
+      const result = methods.add(objects, () => new Obj('Test obj'));
+      expect(result).toHaveLength(objects.length + 1);
+      expect(result[result.length - 1].value).toBe('Test obj');
+    });
+  });
+
+  describe('remove', () => {
+    it('removes object from array', () => {
+      const result = methods.remove(objects, 0);
+      expect(result).toHaveLength(objects.length - 1);
+    });
+
+    it("doesn't remove anything when index is incorrect", () => {
+      expect(methods.remove(objects, 2)).toHaveLength(objects.length);
+    });
+  });
+});
