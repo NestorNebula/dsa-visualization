c481e91fcd19896498b8263ce392933f6f9dc1ed
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-21 09:49:24 +0100

    feat: add LinkedList page

---
+++ src/pages/data-structures/linked-list/LinkedList.tsx
@@ -0,0 +1,29 @@
+import { useLinkedLists } from '@hooks';
+import Sidebar from '@components/sidebar/Sidebar';
+import LinkedListMain from './linked-list-main/LinkedListMain';
+import { LinkedList as Prototype } from '@components/prototypes';
+import * as S from './LinkedList.styles';
+
+function LinkedList() {
+  const { linkedLists, linkedList } = useLinkedLists();
+
+  return (
+    <S.LinkedList>
+      <Sidebar
+        dataStructures={linkedLists}
+        methods={{
+          active: linkedList.active,
+          set: linkedList.set,
+          add: linkedList.add,
+          remove: linkedList.remove,
+        }}
+        getPrototype={(ds, i) => (
+          <Prototype key={`linked-list${i}`} linkedList={ds} />
+        )}
+      />
+      <LinkedListMain linkedLists={linkedLists} linkedList={linkedList} />
+    </S.LinkedList>
+  );
+}
+
+export default LinkedList;
