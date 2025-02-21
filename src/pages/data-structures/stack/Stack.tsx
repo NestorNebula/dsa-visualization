ebef9d4385e04c69a669db4ddd1b14e49cefe127
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-21 18:09:31 +0100

    feat: add Stack page

---
+++ src/pages/data-structures/stack/Stack.tsx
@@ -0,0 +1,27 @@
+import { useStacks } from '@hooks';
+import Sidebar from '@components/sidebar/Sidebar';
+import StackMain from './stack-main/StackMain';
+import { Stack as Prototype } from '@components/prototypes';
+import * as S from './Stack.styles';
+
+function Stack() {
+  const { stacks, stack } = useStacks();
+
+  return (
+    <S.Stack>
+      <Sidebar
+        dataStructures={stacks}
+        methods={{
+          active: stack.active,
+          set: stack.set,
+          add: stack.add,
+          remove: stack.remove,
+        }}
+        getPrototype={(ds, i) => <Prototype key={`stack-${i}`} stack={ds} />}
+      />
+      <StackMain stacks={stacks} stack={stack} />
+    </S.Stack>
+  );
+}
+
+export default Stack;
