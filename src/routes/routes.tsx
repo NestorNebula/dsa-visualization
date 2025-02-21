9732a9529a88b93bc0c03d6b3d2c38b9410c44ea
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-21 10:04:22 +0100

    feat: add LinkedList page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,7 @@ import Navbar from '@pages/navbar/Navbar';
 import App from '../App';
 import DataStructures from '@pages/data-structures/DataStructures';
 import Array from '@pages/data-structures/array/Array';
+import LinkedList from '@pages/data-structures/linked-list/LinkedList';
 import Error from '@pages/error/Error';
 import Sperror from 'sperror';
 
@@ -23,6 +24,10 @@ const routes: RouteObject[] = [
       {
         path: '/data-structures/array',
         element: <Array />,
+      },
+      {
+        path: '/data-structures/linked-list',
+        element: <LinkedList />,
       },
       {
         path: '*',
