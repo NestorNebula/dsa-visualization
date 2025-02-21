981f29fae9e51824d9fc27752d73ec2320c9e459
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-21 16:14:20 +0100

    feat: add HashMap page

---
+++ src/pages/data-structures/hashmap/HashMap.styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const HashMap = styled.main``;
+
+export { HashMap };
---
+++ src/pages/data-structures/hashmap/HashMap.tsx
@@ -0,0 +1,29 @@
+import { useHashMaps } from '@hooks';
+import Sidebar from '@components/sidebar/Sidebar';
+import HashMapMain from './hashmap-main/HashMapMain';
+import { HashMap as Prototype } from '@components/prototypes';
+import * as S from './HashMap.styles';
+
+function HashMap() {
+  const { hashMaps, hashMap } = useHashMaps();
+
+  return (
+    <S.HashMap>
+      <Sidebar
+        dataStructures={hashMaps}
+        methods={{
+          active: hashMap.active,
+          set: hashMap.set,
+          add: hashMap.add,
+          remove: hashMap.remove,
+        }}
+        getPrototype={(ds, i) => (
+          <Prototype key={`hashmap-${i}`} hashMap={ds} />
+        )}
+      />
+      <HashMapMain hashMaps={hashMaps} hashMap={hashMap} />
+    </S.HashMap>
+  );
+}
+
+export default HashMap;
