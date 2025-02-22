94e8dea61444ea19a594ecb02e5128e8a877b633
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-22 10:26:10 +0100

    feat: set queue.queue as not private
    Setting queue.queue as private caused problems for local storage

--- src/services/data-structures/queue/queue.ts
+++ src/services/data-structures/queue/queue.ts
@@ -1,30 +1,30 @@
 import LinkedList from '../linked-list/linked-list';
 
 class Queue<Type = any> {
-  #queue: LinkedList<Type> = new LinkedList();
-  head = this.#queue.head;
-  tail = this.#queue.tail;
+  queue: LinkedList<Type> = new LinkedList();
+  head = this.queue.head;
+  tail = this.queue.tail;
   add(value: Type): void {
-    this.#queue.add(value);
-    this.head = this.#queue.head;
-    this.tail = this.#queue.tail;
+    this.queue.add(value);
+    this.head = this.queue.head;
+    this.tail = this.queue.tail;
   }
   remove(): Type | undefined {
-    if (!this.#queue.head) return;
-    const node = this.#queue.head;
-    if (this.#queue.head === this.#queue.tail) {
-      this.#queue.head = this.#queue.tail = null;
+    if (!this.queue.head) return;
+    const node = this.queue.head;
+    if (this.queue.head === this.queue.tail) {
+      this.queue.head = this.queue.tail = null;
     } else {
-      this.#queue.head = this.#queue.head.next;
+      this.queue.head = this.queue.head.next;
     }
-    this.head = this.#queue.head;
-    this.tail = this.#queue.tail;
+    this.head = this.queue.head;
+    this.tail = this.queue.tail;
     return node.value;
   }
 
   constructor(queue?: Queue) {
     if (queue) {
-      this.#queue = new LinkedList(queue.#queue);
+      this.queue = new LinkedList(queue.queue);
       this.head = queue.head;
       this.tail = queue.tail;
     }
