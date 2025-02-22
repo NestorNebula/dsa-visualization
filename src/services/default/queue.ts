14578c4d1227539fb812caca8753150275b358b8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-22 11:09:27 +0100

    feat: update default queues

--- src/services/default/queue.ts
+++ src/services/default/queue.ts
@@ -5,9 +5,9 @@ queueOne.add(1);
 queueOne.add(2);
 queueOne.add(3);
 const queueTwo = new Queue();
-queueOne.add('Alice');
-queueOne.add('Bob');
-queueOne.add('Charlie');
+queueTwo.add('Alice');
+queueTwo.add('Bob');
+queueTwo.add('Charlie');
 
 const queues: Queue[] = [queueOne, queueTwo];
 
