d99c61bf59d0ad6ee809fae516a9f842264d3215
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-22 11:07:18 +0100

    feat: add Queue page

---
+++ src/pages/data-structures/queue/Queue.tsx
@@ -0,0 +1,27 @@
+import { useQueues } from '@hooks';
+import Sidebar from '@components/sidebar/Sidebar';
+import QueueMain from './queue-main/QueueMain';
+import { Queue as Prototype } from '@components/prototypes';
+import * as S from './Queue.styles';
+
+function Queue() {
+  const { queues, queue } = useQueues();
+
+  return (
+    <S.Queue>
+      <Sidebar
+        dataStructures={queues}
+        methods={{
+          active: queue.active,
+          set: queue.set,
+          add: queue.add,
+          remove: queue.remove,
+        }}
+        getPrototype={(ds, i) => <Prototype key={`queue-${i}`} queue={ds} />}
+      />
+      <QueueMain queues={queues} queue={queue} />
+    </S.Queue>
+  );
+}
+
+export default Queue;
