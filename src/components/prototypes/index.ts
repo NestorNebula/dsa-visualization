299447492e5bf4b2ff79714ac374baa5f9b10415
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-23 10:22:45 +0100

    feat: export TreePrototype from index file

--- src/components/prototypes/index.ts
+++ src/components/prototypes/index.ts
@@ -3,5 +3,6 @@ import HashMap from './hashmap/HashMapPrototype';
 import LinkedList from './linked-list/LinkedListPrototype';
 import Queue from './queue/QueuePrototype';
 import Stack from './stack/StackPrototype';
+import Tree from './tree/TreePrototype';
 
-export { Array, HashMap, LinkedList, Queue, Stack };
+export { Array, HashMap, LinkedList, Queue, Stack, Tree };
