55c90930d3ef2105f183c07e3395a0a7d2dcb2ef
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-27 15:33:39 +0100

    feat: export Graph prototype from index file

--- src/components/prototypes/index.ts
+++ src/components/prototypes/index.ts
@@ -1,8 +1,9 @@
 import Array from './array/ArrayPrototype';
+import Graph from './graph/GraphPrototype';
 import HashMap from './hashmap/HashMapPrototype';
 import LinkedList from './linked-list/LinkedListPrototype';
 import Queue from './queue/QueuePrototype';
 import Stack from './stack/StackPrototype';
 import Tree from './tree/TreePrototype';
 
-export { Array, HashMap, LinkedList, Queue, Stack, Tree };
+export { Array, Graph, HashMap, LinkedList, Queue, Stack, Tree };
