6014ca40935305218a11b59113cff7a49014b158
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-02-27 08:12:31 +0100

    feat: add Heap page

---
+++ src/pages/data-structures/heap/Heap.styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const Heap = styled.main``;
+
+export { Heap };
---
+++ src/pages/data-structures/heap/Heap.tsx
@@ -0,0 +1,27 @@
+import { useHeaps } from '@hooks';
+import Sidebar from '@components/sidebar/Sidebar';
+import { Tree as Prototype } from '@components/prototypes';
+import HeapMain from './heap-main/HeapMain';
+import * as S from './Heap.styles';
+
+function Heap() {
+  const { heaps, heap } = useHeaps();
+
+  return (
+    <S.Heap>
+      <Sidebar
+        dataStructures={heaps}
+        methods={{
+          active: heap.active,
+          set: heap.set,
+          add: heap.add,
+          remove: heap.remove,
+        }}
+        getPrototype={(ds, i) => <Prototype key={`heap-${i}`} tree={ds} />}
+      />
+      <HeapMain heaps={heaps} heap={heap} />
+    </S.Heap>
+  );
+}
+
+export default Heap;
