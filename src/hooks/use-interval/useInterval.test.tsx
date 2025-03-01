fbfac553366bca746997704a2c05fdd227054d6b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-01 10:35:34 +0100

    test: add test for stop method

--- src/hooks/use-interval/useInterval.test.tsx
+++ src/hooks/use-interval/useInterval.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from 'vitest';
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import useInterval from './useInterval';
 
 const mockCallback = vi.fn();
@@ -21,36 +21,49 @@ describe('useinterval', () => {
   it('calls callback function every second (default)', async () => {
     renderHook(() => useInterval(mockCallback));
     expect(
-      await testPromise(
-        () => expect(mockCallback).toHaveBeenCalledTimes(2),
-        2000
-      )
+      await testPromise(() => {
+        expect(mockCallback).toHaveBeenCalledTimes(2);
+      }, 2050)
     ).toBeTruthy();
   });
 
   it('calls callback function with custom delay as interval', async () => {
     renderHook(() => useInterval(mockCallback, 100));
     expect(
-      await testPromise(
-        () => expect(mockCallback).toHaveBeenCalledTimes(10),
-        1000
-      )
+      await testPromise(() => {
+        expect(mockCallback).toHaveBeenCalledTimes(10);
+      }, 1050)
     ).toBeTruthy();
   });
 
-  it('updates delay between callbacks', async () => {
-    const { result } = renderHook(() => useInterval(mockCallback));
-    expect(
-      await testPromise(() => {
-        expect(mockCallback).toHaveBeenCalledTimes(1);
-        result.current.setDelay(100);
-      }, 1000)
-    ).toBeTruthy();
-    expect(
-      await testPromise(
-        () => expect(mockCallback).toHaveBeenCalledTimes(11),
-        1000
-      )
-    ).toBeTruthy();
+  describe('setdelay', () => {
+    it('updates delay between callbacks', async () => {
+      const { result } = renderHook(() => useInterval(mockCallback));
+      expect(
+        await testPromise(() => {
+          expect(mockCallback).toHaveBeenCalledTimes(1);
+          act(() => result.current.setDelay(100));
+        }, 1000)
+      ).toBeTruthy();
+      expect(
+        await testPromise(
+          () => expect(mockCallback).toHaveBeenCalledTimes(10),
+          1000
+        )
+      ).toBeTruthy();
+    });
+  });
+
+  describe('stop', () => {
+    it('stops interval', async () => {
+      const { result } = renderHook(() => useInterval(mockCallback));
+      act(() => result.current.stop());
+
+      expect(
+        await testPromise(() => {
+          expect(mockCallback).not.toHaveBeenCalled();
+        }, 1000)
+      ).toBeTruthy();
+    });
   });
 });
