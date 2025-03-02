315c1d3c8f78c37798595d82c10e9c3811128f8c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-02 14:45:46 +0100

    feat: add forward, pause and play icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,10 +1,13 @@
 import arrowLeft from './arrow-left.svg';
 import check from './check.svg';
 import close from './close.svg';
+import forward from './forward.svg';
 import github from './github.svg';
 import icon from './icon.png';
 import linkArrow from './link-arrow.svg';
 import minus from './minus.svg';
+import pause from './pause.svg';
+import play from './play.svg';
 import plus from './plus.svg';
 import resource from './resource.svg';
 import revert from './revert.svg';
@@ -13,10 +16,13 @@ export {
   arrowLeft,
   check,
   close,
+  forward,
   github,
   icon,
   linkArrow,
   minus,
+  pause,
+  play,
   plus,
   resource,
   revert,
