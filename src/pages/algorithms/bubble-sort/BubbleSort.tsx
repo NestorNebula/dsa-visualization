0d8cc330cf863c2535ac7fce9b6709856241e092
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-02 16:10:40 +0100

    feat: add BubbleSort page

---
+++ src/pages/algorithms/bubble-sort/BubbleSort.tsx
@@ -0,0 +1,25 @@
+import { useArrays } from '@hooks';
+import Sidebar from '@components/sidebar/Sidebar';
+import BubbleSortMain from './bubble-sort-main/BubbleSortMain';
+import { Array as Prototype } from '@components/prototypes';
+import * as S from './BubbleSort.styles';
+
+function BubbleSort() {
+  const { arrays, array } = useArrays();
+
+  return (
+    <S.BubbleSort>
+      <Sidebar
+        dataStructures={arrays}
+        methods={{ active: array.active, set: array.set }}
+        getPrototype={(ds, i) => <Prototype key={`array-${i}`} array={ds} />}
+      />
+      <BubbleSortMain
+        key={`bubble-sort-${array.active}`}
+        array={arrays[array.active]}
+      />
+    </S.BubbleSort>
+  );
+}
+
+export default BubbleSort;
