d6f91314e3df6399f438238dd839f33ffcd3f3d9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-02 15:15:15 +0100

    feat: add SpeedBar component

---
+++ src/components/speed-bar/SpeedBar.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const SpeedBar = styled.div``;
+const Options = styled.div``;
+const Button = styled.button``;
+const Status = styled.div``;
+
+export { SpeedBar, Options, Button, Status };
---
+++ src/components/speed-bar/SpeedBar.tsx
@@ -0,0 +1,31 @@
+import { forward, pause, play } from '@assets/icons';
+import * as S from './SpeedBar.styles';
+
+type Status = 'Active' | 'Faster' | 'Paused' | 'Done' | 'Failed';
+
+function SpeedBar({
+  status,
+  setStatus,
+}: {
+  status: Status;
+  setStatus: (status: Status) => void;
+}) {
+  return (
+    <S.SpeedBar>
+      <S.Options>
+        <S.Button aria-label="pause" onClick={() => setStatus('Paused')}>
+          <img src={pause} alt="Pause" />
+        </S.Button>
+        <S.Button aria-label="play" onClick={() => setStatus('Active')}>
+          <img src={play} alt="Play" />
+        </S.Button>
+        <S.Button aria-label="faster" onClick={() => setStatus('Faster')}>
+          <img src={forward} alt="Forward" />
+        </S.Button>
+      </S.Options>
+      <S.Status>{status}</S.Status>
+    </S.SpeedBar>
+  );
+}
+
+export default SpeedBar;
