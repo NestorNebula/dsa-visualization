7c53b850ef8d1436051ff2a9cac0a75132f24e57
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-03 16:08:58 +0100

    feat: add InsertionSort page

---
+++ src/pages/algorithms/insertion-sort/InsertionSort.tsx
@@ -0,0 +1,25 @@
+import { useArrays } from '@hooks';
+import Sidebar from '@components/sidebar/Sidebar';
+import InsertionSortMain from './insertion-sort-main/InsertionSortMain';
+import { Array as Prototype } from '@components/prototypes';
+import * as S from './InsertionSort.styles';
+
+function InsertionSort() {
+  const { arrays, array } = useArrays();
+
+  return (
+    <S.InsertionSort>
+      <Sidebar
+        dataStructures={arrays}
+        methods={{ active: array.active, set: array.set }}
+        getPrototype={(ds, i) => <Prototype key={`array-${i}`} array={ds} />}
+      />
+      <InsertionSortMain
+        key={`insertion-sort-${array.active}`}
+        array={arrays[array.active]}
+      />
+    </S.InsertionSort>
+  );
+}
+
+export default InsertionSort;
