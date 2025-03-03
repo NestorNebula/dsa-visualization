bd5990b6f3ef3fb178645c4d6936b3105eeff1c8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-03 10:49:40 +0100

    feat: add SelectionSortMain page

---
+++ src/pages/algorithms/selection-sort/selection-sort-main/SelectionSortMain.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import { useSelectionSort } from '@hooks';
+import DSAHeader from '@components/dsa-header/DSAHeader';
+import SpeedBar, { type Status } from '@components/speed-bar/SpeedBar';
+import { Array as Prototype } from '@components/prototypes';
+import * as S from './SelectionSortMain.styles';
+import type { Array } from '@services/data-structures';
+
+function SelectionSortMain({ array }: { array: Array }) {
+  const [status, setStatus] = useState<Status>('Paused');
+  const updateStatus = (status: Status) => {
+    if (['Done', 'Failed'].includes(status)) return;
+    for (let i = 0; i < array.length; i++) {
+      if (!Number.isInteger(array[i])) return;
+    }
+    setStatus(status);
+  };
+
+  const selectionSort = useSelectionSort(array);
+
+  useEffect(() => {
+    switch (status) {
+      case 'Active':
+        selectionSort.setDelay(1000);
+        selectionSort.start();
+        break;
+      case 'Faster':
+        selectionSort.setDelay(500);
+        break;
+      case 'Paused':
+        selectionSort.stop();
+        break;
+      case 'Failed':
+        selectionSort.stop();
+        break;
+    }
+  }, [status]);
+
+  useEffect(() => {
+    if (selectionSort.done) {
+      setStatus('Done');
+    }
+  }, [selectionSort.done]);
+
+  return (
+    <S.SelectionSortMain>
+      <DSAHeader
+        title="Selection Sort"
+        resource="https://www.geeksforgeeks.org/selection-sort-algorithm-2/"
+      />
+      <SpeedBar status={status} setStatus={updateStatus} />
+      <Prototype array={selectionSort.sortedArray} />
+    </S.SelectionSortMain>
+  );
+}
+
+export default SelectionSortMain;
