b910717b19460a2666b73b2c0fa90b87c62d3c4e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-04 18:08:53 +0100

    feat: add styles to Navbar page

--- src/pages/navbar/Navbar.styles.ts
+++ src/pages/navbar/Navbar.styles.ts
@@ -1,9 +1,40 @@
 import styled from 'styled-components';
 
-const Navbar = styled.nav``;
-const NavItem = styled.li``;
-const Prev = styled.button``;
+const Navbar = styled.nav`
+  font-size: var(--fs-l);
+  padding: 1rem 3rem;
+  text-align: center;
+
+  & > ul {
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: center;
+
+    & > * {
+      width: 100%;
+      display: grid;
+      justify-content: center;
+    }
+  }
+`;
+const NavItem = styled.li`
+  &:first-of-type {
+    justify-content: start;
+  }
+
+  &:last-of-type {
+    justify-content: end;
+  }
+`;
+const Prev = styled.button`
+  & > img {
+    max-width: 5rem;
+  }
+`;
 const Title = styled.div``;
-const Icon = styled.img``;
+const Icon = styled.img`
+  max-width: 10rem;
+`;
 
 export { Navbar, NavItem, Prev, Title, Icon };
