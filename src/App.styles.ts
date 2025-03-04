1f0d853dbc822352b2c540d22789bc666bceceec
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-04 18:24:38 +0100

    feat: add styles to App

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,8 +1,38 @@
 import styled from 'styled-components';
 
-const App = styled.main``;
-const Header = styled.header``;
-const Main = styled.main``;
-const Links = styled.div``;
+const App = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  font-size: var(--fs-m);
+`;
+const Header = styled.header`
+  text-align: center;
+`;
+const Main = styled.main`
+  line-height: 2;
+  padding: 0 5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+const Links = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-items: center;
+
+  & > div {
+    grid-column: 1 / 3;
+  }
+
+  & > a {
+    text-decoration: underline;
+    color: ${(props) => props.theme.darkBlue};
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+`;
 
 export { App, Header, Main, Links };
