3e43da5ba999b44dc4b43f4f48593bd7c8c9023c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-04 17:14:07 +0100

    feat: add data structures and algorithms images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,36 @@
+import array from './array.png';
+import binarySearch from './binary-search.png';
+import binaryTree from './binary-tree.png';
+import bubbleSort from './bubble-sort.png';
+import graph from './graph.png';
+import hashMap from './hashmap.png';
+import heap from './heap.png';
+import insertionSort from './insertion-sort.png';
+import linearSearch from './linear-search.png';
+import linkedList from './linked-list.png';
+import mergeSort from './merge-sort.png';
+import queue from './queue.png';
+import selectionSort from './selection-sort.png';
+import stack from './stack.png';
+
+const dataStructures = {
+  array,
+  binaryTree,
+  graph,
+  hashMap,
+  heap,
+  linkedList,
+  queue,
+  stack,
+};
+
+const algorithms = {
+  binarySearch,
+  bubbleSort,
+  insertionSort,
+  linearSearch,
+  mergeSort,
+  selectionSort,
+};
+
+export { dataStructures, algorithms };
