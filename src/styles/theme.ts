e30df9d3d188a5e9ee4183850011fd06ce36329f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-04 17:41:37 +0100

    feat: add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  darkBlue: '#107B96',
+  lightBlue: '#79DDF0',
+  darkRed: '#982B10',
+  lightRed: '#F08E7A',
+  white: '#FAF5F4',
+  green: '#7DD4AA',
+};
+
+export default theme;
