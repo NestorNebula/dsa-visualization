7c7d6d2882407af6200b7b3f299a755481f62b7c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-04 17:22:07 +0100

    feat: add GlobalStyle

---
+++ src/styles/global-style.ts
@@ -0,0 +1,73 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+    font-family: inherit;
+  }
+
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  img,
+  picture,
+  video,
+  canvas {
+    display: block;
+    max-width: 100%;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+  }
+
+  :root {
+    font-size: 62.5%;
+    --fs-l: clamp(2.25rem, 2.5vw + 1rem, 5rem);
+    --fs-m: clamp(1.25rem, 1.5vw + 0.25rem, 2.5rem);
+    --fs-s: clamp(0.85rem, 1vw + 0.05rem, 1.75rem);
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    border: none;
+    background-color: inherit;
+    padding: 0;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  ul {
+    padding: 0;
+    list-style: none;
+  }
+`;
+
+export default GlobalStyle;
