a79c5a4ad2612412a3f4882a3de662adddcdb508
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-04 17:44:15 +0100

    feat: add GlobalStyle and Theme to the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,18 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import routes from '@routes/routes';
 import Footer from '@pages/footer/Footer';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@styles/global-style';
+import theme from '@styles/theme';
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <Footer />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+      <Footer />
+    </ThemeProvider>
   </StrictMode>
 );
