fb69f8fedcd1609298387e14042a78d758f3fd2c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-04 10:17:30 +0100

    feat: add MergeSort page

---
+++ src/pages/algorithms/merge-sort/MergeSort.tsx
@@ -0,0 +1,25 @@
+import { useArrays } from '@hooks';
+import Sidebar from '@components/sidebar/Sidebar';
+import MergeSortMain from './merge-sort-main/MergeSortMain';
+import { Array as Prototype } from '@components/prototypes';
+import * as S from './MergeSort.styles';
+
+function MergeSort() {
+  const { arrays, array } = useArrays();
+
+  return (
+    <S.MergeSort>
+      <Sidebar
+        dataStructures={arrays}
+        methods={{ active: array.active, set: array.set }}
+        getPrototype={(ds, i) => <Prototype key={`array-${i}`} array={ds} />}
+      />
+      <MergeSortMain
+        key={`merge-sort-${array.active}`}
+        array={arrays[array.active]}
+      />
+    </S.MergeSort>
+  );
+}
+
+export default MergeSort;
