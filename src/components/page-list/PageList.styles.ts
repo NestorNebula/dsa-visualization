7c5b5b13d3db5319de9e7b00ecd7422b04d8e03b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-04 18:29:44 +0100

    feat: add styles to PageList

--- src/components/page-list/PageList.styles.ts
+++ src/components/page-list/PageList.styles.ts
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
 
-const PageList = styled.section``;
-const Title = styled.div``;
-const Pages = styled.div``;
+const PageList = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
+const Title = styled.div`
+  font-size: var(--fs-l);
+  text-align: center;
+`;
+const Pages = styled.div`
+  padding: 0 5rem;
+  font-size: var(--fs-m);
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 5rem;
+`;
 
 export { PageList, Title, Pages };
