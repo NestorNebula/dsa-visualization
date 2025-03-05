3cd595cea0c33e935d6cf5fe70cf69607e3b618e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 09:36:17 +0100

    feat: add styles to ArrayMain page

--- src/pages/data-structures/array/array-main/ArrayMain.styles.ts
+++ src/pages/data-structures/array/array-main/ArrayMain.styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-const ArrayMain = styled.section``;
+const ArrayMain = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  font-size: var(--fs-m);
+`;
 
 export { ArrayMain };
