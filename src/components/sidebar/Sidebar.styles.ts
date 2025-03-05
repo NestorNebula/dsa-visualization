3c54daf5da305429e233940f2729b8d9e16978ba
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 09:35:52 +0100

    feat: add styles to Sidebar

--- src/components/sidebar/Sidebar.styles.ts
+++ src/components/sidebar/Sidebar.styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
-const Sidebar = styled.aside``;
+const Sidebar = styled.aside`
+  margin: 0 2.5rem;
+  padding: 2.5rem;
+  background-color: ${(props) => props.theme.lightGrey};
+  border-radius: 0.5rem;
+`;
 
 export { Sidebar };
