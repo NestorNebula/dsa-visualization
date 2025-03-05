d5abc855aad631d5ffa9f84f9484ef8d6bdf7919
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 07:46:57 +0100

    feat: add styles to PageLink

--- src/components/page-link/PageLink.styles.ts
+++ src/components/page-link/PageLink.styles.ts
@@ -1,7 +1,32 @@
 import styled from 'styled-components';
 
-const PageLink = styled.button``;
-const Title = styled.div``;
+const PageLink = styled.button`
+  padding: 2rem;
+  display: grid;
+  place-content: center;
+  position: relative;
+  transition: transform 0.2s ease-in;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+const Title = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  place-content: center;
+  font-size: var(--fs-l);
+  background-color: rgba(145, 163, 176, 0.4);
+  border-radius: 3rem;
+  transition: background-color 0.5s ease-in, color 0.25s ease-in;
+
+  &:hover {
+    background-color: transparent;
+    color: transparent;
+  }
+`;
 const Icon = styled.img``;
 
 export { PageLink, Title, Icon };
