be16e94568c777cd3a09a50cd09d8e51bb2378e5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 09:34:06 +0100

    feat: add styles to Form

--- src/components/forms/form/Form.styles.ts
+++ src/components/forms/form/Form.styles.ts
@@ -1,6 +1,15 @@
 import styled from 'styled-components';
 
-const Form = styled.form``;
-const SubmitButton = styled.button``;
+const Form = styled.form`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  font-size: var(--fs-m);
+`;
+const SubmitButton = styled.button`
+  & > img {
+    min-width: 3rem;
+  }
+`;
 
 export { Form, SubmitButton };
