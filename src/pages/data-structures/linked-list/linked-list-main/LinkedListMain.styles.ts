68300a0535bc9d6046b32e7745975a3b3d1e2f11
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 10:45:44 +0100

    feat: add styles to LinkedListMain page

--- src/pages/data-structures/linked-list/linked-list-main/LinkedListMain.styles.ts
+++ src/pages/data-structures/linked-list/linked-list-main/LinkedListMain.styles.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
-const LinkedListMain = styled.section``;
+const LinkedListMain = styled.section`
+  display: flex;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  font-size: var(--fs-m);
+`;
 
 export { LinkedListMain };
