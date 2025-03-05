9d2acca2fe0ff3fcd2b29c838b94e061bf3539e3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 11:25:31 +0100

    feat: add styles to LinkedListPrototype

--- src/components/prototypes/linked-list/LinkedListPrototype.styles.ts
+++ src/components/prototypes/linked-list/LinkedListPrototype.styles.ts
@@ -1,6 +1,23 @@
 import styled from 'styled-components';
 
-const LinkedList = styled.div``;
-const Item = styled.div``;
+const LinkedList = styled.div`
+  & > div {
+    padding: 1rem;
+    display: flex;
+  }
+`;
+const Item = styled.div`
+  display: flex;
+  flex-shrink: 0;
+
+  & > div {
+    padding: 0.5rem 1.5rem;
+    border: 1px solid black;
+  }
+
+  & > img {
+    width: 3rem;
+  }
+`;
 
 export { LinkedList, Item };
