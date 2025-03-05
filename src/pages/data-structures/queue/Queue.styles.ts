ca67754ffb950c06b328d833f2f9a4f646ce620b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 14:42:46 +0100

    feat: add styles to Queue pages

--- src/pages/data-structures/queue/Queue.styles.ts
+++ src/pages/data-structures/queue/Queue.styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
+import { CommonDSA } from '@styles/common-styles';
 
-const Queue = styled.main``;
+const Queue = styled(CommonDSA)``;
 
 export { Queue };
--- src/pages/data-structures/queue/queue-main/QueueMain.styles.ts
+++ src/pages/data-structures/queue/queue-main/QueueMain.styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
+import { CommonDSAMain } from '@styles/common-styles';
 
-const QueueMain = styled.section``;
+const QueueMain = styled(CommonDSAMain)``;
 
 export { QueueMain };
