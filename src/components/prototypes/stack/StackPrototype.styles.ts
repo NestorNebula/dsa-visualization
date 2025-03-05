2b1046c12f13a61648254d93e26182b093c3f25f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 14:35:57 +0100

    feat: add styles to StackPrototype

--- src/components/prototypes/stack/StackPrototype.styles.ts
+++ src/components/prototypes/stack/StackPrototype.styles.ts
@@ -1,5 +1,26 @@
 import styled from 'styled-components';
 
-const StackPrototype = styled.div``;
+const StackPrototype = styled.div`
+  & > div {
+    border: 1px solid black;
+    border-top: none;
+    display: flex;
+    flex-direction: column-reverse;
+    min-width: 10rem;
+    padding: 1rem;
+
+    & > div {
+      text-align: center;
+      width: 100%;
+      padding: 0.5rem;
+      border: 1px solid black;
+    }
+  }
+
+  & > div:not(:has(div))::after {
+    position: relative;
+    content: 'Empty Stack';
+  }
+`;
 
 export { StackPrototype };
