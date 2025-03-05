a2452af834f2e43f998f22f1f7f05fcc2591eac2
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 11:24:27 +0100

    feat: update Sidebar styles

--- src/components/sidebar/Sidebar.styles.ts
+++ src/components/sidebar/Sidebar.styles.ts
@@ -2,9 +2,14 @@ import styled from 'styled-components';
 
 const Sidebar = styled.aside`
   margin: 0 2.5rem;
-  padding: 2.5rem;
+  padding: 1rem;
   background-color: ${(props) => props.theme.lightGrey};
   border-radius: 0.5rem;
+  overflow: scroll;
+
+  @media (min-width: 800px) {
+    padding: 2.5rem;
+  }
 `;
 
 export { Sidebar };
