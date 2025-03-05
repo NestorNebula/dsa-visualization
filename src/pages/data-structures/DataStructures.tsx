73861d7e7257d243861435d5b4d9dfe0910454fd
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 07:47:36 +0100

    feat: add images to DataStructures

--- src/pages/data-structures/DataStructures.tsx
+++ src/pages/data-structures/DataStructures.tsx
@@ -1,19 +1,32 @@
 import PageList from '@components/page-list/PageList';
 import PageLink from '@components/page-link/PageLink';
+import { dataStructures } from '@assets/images';
 import * as S from './DataStructures.styles';
 
 function DataStructures() {
   return (
     <S.DataStructures>
       <PageList title="Data Structures">
-        <PageLink title="Array" icon="TBD" path="array" />
-        <PageLink title="HashMap" icon="TBD" path="hashmap" />
-        <PageLink title="Stack" icon="TBD" path="stack" />
-        <PageLink title="Queue" icon="TBD" path="queue" />
-        <PageLink title="Linked List" icon="TBD" path="linked-list" />
-        <PageLink title="Binary Tree" icon="TBD" path="binary-tree" />
-        <PageLink title="Heap" icon="TBD" path="heap" />
-        <PageLink title="Graph" icon="TBD" path="graph" />
+        <PageLink title="Array" icon={dataStructures.array} path="array" />
+        <PageLink
+          title="HashMap"
+          icon={dataStructures.hashMap}
+          path="hashmap"
+        />
+        <PageLink title="Stack" icon={dataStructures.stack} path="stack" />
+        <PageLink title="Queue" icon={dataStructures.queue} path="queue" />
+        <PageLink
+          title="Linked List"
+          icon={dataStructures.linkedList}
+          path="linked-list"
+        />
+        <PageLink
+          title="Binary Tree"
+          icon={dataStructures.binaryTree}
+          path="binary-tree"
+        />
+        <PageLink title="Heap" icon={dataStructures.heap} path="heap" />
+        <PageLink title="Graph" icon={dataStructures.graph} path="graph" />
       </PageList>
     </S.DataStructures>
   );
