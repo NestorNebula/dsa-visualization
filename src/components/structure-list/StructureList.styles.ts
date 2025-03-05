40bc6ab247ab055dde586a01336c8eafcdb488de
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-05 09:35:14 +0100

    feat: add styles to StructureList

--- src/components/structure-list/StructureList.styles.ts
+++ src/components/structure-list/StructureList.styles.ts
@@ -1,9 +1,30 @@
 import styled from 'styled-components';
 
-const StructureList = styled.section``;
-const Structure = styled.div``;
+const StructureList = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  font-size: var(--fs-s);
+`;
+const Structure = styled.div`
+  display: grid;
+  width: 100%;
+  gap: 1rem;
+  justify-items: center;
+`;
 const StructureButton = styled.button``;
-const Delete = styled.button``;
-const Add = styled.button``;
+const Delete = styled.button`
+  justify-self: end;
+
+  & > img {
+    min-width: 1.5rem;
+  }
+`;
+const Add = styled.button`
+  & > img {
+    justify-self: center;
+    max-width: 2rem;
+  }
+`;
 
 export { StructureList, Structure, StructureButton, Delete, Add };
