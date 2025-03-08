d7d2a5fd93ba4e432c2c9dd1a64838f3692a6791
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-08 09:46:19 +0100

    feat: add images to Algorithms page

--- src/pages/algorithms/Algorithms.tsx
+++ src/pages/algorithms/Algorithms.tsx
@@ -1,19 +1,44 @@
 import PageList from '@components/page-list/PageList';
 import PageLink from '@components/page-link/PageLink';
+import { algorithms } from '@assets/images';
 import * as S from './Algorithms.styles';
 
 function Algorithms() {
   return (
     <S.Algorithms>
       <PageList title="Sorting Algorithms">
-        <PageLink title="Bubble Sort" icon="TBD" path="bubble-sort" />
-        <PageLink title="Selection Sort" icon="TBD" path="selection-sort" />
-        <PageLink title="Insertion Sort" icon="TBD" path="insertion-sort" />
-        <PageLink title="Merge Sort" icon="TBD" path="merge-sort" />
+        <PageLink
+          title="Bubble Sort"
+          icon={algorithms.bubbleSort}
+          path="bubble-sort"
+        />
+        <PageLink
+          title="Selection Sort"
+          icon={algorithms.selectionSort}
+          path="selection-sort"
+        />
+        <PageLink
+          title="Insertion Sort"
+          icon={algorithms.insertionSort}
+          path="insertion-sort"
+        />
+        <PageLink
+          title="Merge Sort"
+          icon={algorithms.mergeSort}
+          path="merge-sort"
+        />
       </PageList>
       <PageList title="Searching Algorithms">
-        <PageLink title="Linear Search" icon="TBD" path="linear-search" />
-        <PageLink title="Binary Search" icon="TBD" path="binary-search" />
+        <PageLink
+          title="Linear Search"
+          icon={algorithms.linearSearch}
+          path="linear-search"
+        />
+        <PageLink
+          title="Binary Search"
+          icon={algorithms.binarySearch}
+          path="binary-search"
+        />
       </PageList>
     </S.Algorithms>
   );
