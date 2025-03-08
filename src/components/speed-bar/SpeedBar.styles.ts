78d65cd6f36d77701582f8ab75482ceb078801d1
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-08 09:50:57 +0100

    feat: add styles to SpeedBar

--- src/components/speed-bar/SpeedBar.styles.ts
+++ src/components/speed-bar/SpeedBar.styles.ts
@@ -1,8 +1,24 @@
 import styled from 'styled-components';
 
-const SpeedBar = styled.div``;
-const Options = styled.div``;
-const Button = styled.button``;
+const SpeedBar = styled.div`
+  background-color: ${(props) => props.theme.lightGrey};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+  border-radius: 1rem;
+`;
+const Options = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+const Button = styled.button`
+  & > img {
+    min-width: 5rem;
+  }
+`;
 const Status = styled.div``;
 
 export { SpeedBar, Options, Button, Status };
