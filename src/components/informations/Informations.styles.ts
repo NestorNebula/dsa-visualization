85de2b363f5c672c89412bbf103dd61a754be31d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-09 11:50:38 +0100

    feat: add styles to Informations

--- src/components/informations/Informations.styles.ts
+++ src/components/informations/Informations.styles.ts
@@ -1,7 +1,34 @@
 import styled from 'styled-components';
 
-const Informations = styled.div``;
-const Title = styled.div``;
-const Content = styled.div``;
+const Informations = styled.div`
+  background-color: ${(props) => props.theme.darkRed};
+  color: ${(props) => props.theme.white};
+  font-size: var(--fs-s);
+  max-width: max(50%, 300px);
+  padding: 2rem;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  position: relative;
+`;
+const Title = styled.div`
+  font-size: var(--fs-m);
+`;
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+const CloseButton = styled.button`
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
 
-export { Informations, Title, Content };
+  & > img {
+    filter: invert();
+    min-width: 2rem;
+  }
+`;
+
+export { Informations, Title, Content, CloseButton };
