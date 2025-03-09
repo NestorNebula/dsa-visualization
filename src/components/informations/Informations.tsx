7902ddd89377e75a17d6e7cd62b4ce4397c59e0b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-09 11:49:15 +0100

    feat: add CloseButton

--- src/components/informations/Informations.tsx
+++ src/components/informations/Informations.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+import { close } from '@assets/icons';
 import * as S from './Informations.styles';
 import type { JSX } from 'react';
 
 function Informations({ children }: { children: JSX.Element | JSX.Element[] }) {
-  return (
+  const [closed, setClosed] = useState(false);
+
+  return closed ? (
+    <></>
+  ) : (
     <S.Informations>
       <S.Title>Did you know?</S.Title>
       <S.Content>{children}</S.Content>
+      <S.CloseButton
+        aria-label="close informations"
+        onClick={() => setClosed(true)}
+      >
+        <img src={close} alt="close" />
+      </S.CloseButton>
     </S.Informations>
   );
 }
