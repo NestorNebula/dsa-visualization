b05f5aaf3a1fb3de1e9bc9eb21d6b160bfcadf63
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-09 11:43:46 +0100

    test: add test for close button

--- src/components/informations/Informations.test.tsx
+++ src/components/informations/Informations.test.tsx
@@ -1,5 +1,6 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Informations from './Informations';
 
 beforeEach(() => {
@@ -17,5 +18,11 @@ describe('informations', () => {
 
   it('renders children content', () => {
     expect(screen.queryByText('Test')).toBeInTheDocument();
+  });
+
+  it("doesn't display anything after close button click", async () => {
+    const user = userEvent.setup();
+    await user.click(screen.getByRole('button', { name: /close/i }));
+    expect(screen.queryByText('Test')).not.toBeInTheDocument();
   });
 });
