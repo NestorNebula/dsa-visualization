b0873640beb75526fcde8c998ce41d74fd7fb1f0
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-03-10 08:20:55 +0100

    feat: add styles to Error page

--- src/pages/error/Error.styles.ts
+++ src/pages/error/Error.styles.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
 
-const Error = styled.main``;
+const Error = styled.main`
+  display: grid;
+  place-content: center;
+  text-align: center;
+  font-size: var(--fs-l);
+  min-height: 100vh;
+
+  & > a {
+    color: ${(props) => props.theme.darkBlue};
+    text-decoration: underline;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  & + footer {
+    position: relative;
+    top: 10vh;
+  }
+`;
 
 export { Error };
